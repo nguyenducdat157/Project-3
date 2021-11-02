b77ca495916e810cd4382a49cc37a5dd614929f2
hustchihieu143 <chihieusky@gmail.com>
2021-11-02 16:01:17 +0700

    custom code be

--- backend/src/routes/post.js
+++ backend/src/routes/post.js
@@ -1,40 +1,26 @@
-
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const controller = require("../controller/post");
-const {requireSignIn} = require("../middleware")
+const controller = require('../controller/post');
+const { requireSignIn } = require('../middleware');
 
-const multer = require("multer");
-const shortid = require("shortid");
-const path = require( "path" );
+const multer = require('multer');
+const shortid = require('shortid');
+const path = require('path');
 
-const storage = multer.diskStorage( {
-    destination: function (req, file, cb) {
-        cb(null, path.join(path.dirname(__dirname), "uploads"));
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname.replace(/\s/g, ''));
-    },
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), 'uploads'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname.replace(/\s/g, ''));
+  },
 });
 const upload = multer({ storage });
 
-router.post(
-    "/create-post",
-    requireSignIn,
-    upload.array("pictures"),
-    controller.createPost
-);
+router.post('/create-post', requireSignIn, upload.array('pictures'), controller.createPost);
 
-router.get(
-    "/get-posts",
-    requireSignIn,
-    controller.getPosts
-)
+router.get('/get-posts', requireSignIn, controller.getPosts);
 
-router.delete(
-    "/remove-post/:id",
-    requireSignIn,
-    controller.removePost
-)
+router.delete('/remove-post/:id', requireSignIn, controller.removePost);
 
 module.exports = router;
