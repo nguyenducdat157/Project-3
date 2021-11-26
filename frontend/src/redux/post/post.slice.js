a44240261b65a537f13f6f6ebdae0d06498fbd0b
hustchihieu143 <chihieusky@gmail.com>
2021-11-27 00:11:43 +0700

    show post profile

--- frontend/src/redux/post/post.slice.js
+++ frontend/src/redux/post/post.slice.js
@@ -18,10 +18,17 @@ export const reactApi = createAsyncThunk('post/like', async (params) => {
 });
 
 export const commentApi = createAsyncThunk('post/comment', async (data) => {
- 
   try {
-    const {postId, userId, content} = await data;
-    return await axiosInstance.post(`/api/post/comment/${postId}`, {userId , content});
+    const { postId, userId, content } = await data;
+    return await axiosInstance.post(`/api/post/comment/${postId}`, { userId, content });
+  } catch (error) {
+    return error;
+  }
+});
+
+export const getPostMe = createAsyncThunk('post/get-post-for-me', async () => {
+  try {
+    return await axiosInstance.get(`/api/post/get-post-for-me`);
   } catch (error) {
     return error;
   }
@@ -31,13 +38,26 @@ const initialState = {
   loading: false,
   error: '',
   post: { code: 0, data: {} },
+  postOfMe: { code: 0, data: {} },
 };
 
 const postSlice = createSlice({
   name: 'post',
   initialState: initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [`${getPostMe.pending}`]: (state) => {
+      state.loading = true;
+    },
+    [`${getPostMe.rejected}`]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [`${getPostMe.fulfilled}`]: (state, action) => {
+      state.loading = false;
+      state.postOfMe = action.payload.data;
+    },
+  },
 });
 export const { reducer: postReducer } = postSlice;
 export default postReducer;
